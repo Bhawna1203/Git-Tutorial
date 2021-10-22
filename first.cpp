4db07cab47a9004ad179200920e210433e0de252
Bhawna Chauhan <bhawnabc2002@gmail.com>
2021-10-22 22:20:44 +0530

    added to mine-project

--- first.cpp
+++ first.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     cout<<"THIS IS MY FIRST GIT REPOSITORY NOW IT IS NOT BRANCH IT IS LOGIN SYSTEM";
     cout<<"HOW ARE YOU DID YOU WANNA WANT TO PLAY WITH ME";
-    cout<<"tell me about yourself";
-    cout<<"hey hello";
+    cout<<"tell me about yourself pagal";
+    cout<<"hey hello, i goona missing you";
     return 0;
 }
